2dc5ed7635b4b7b724039dd39007d1a95ddd679b
joaomanoel <joaomanoelc@outlook.com>
2020-12-15 11:57:59 -0300

    new update

--- Quick-azure-bastion-as-a-service/deploy-bastion-service.ps1
+++ Quick-azure-bastion-as-a-service/deploy-bastion-service.ps1
@@ -1,2 +1,2 @@
 New-AzResourceGroup -Name <resource-group-name> -Location <resource-group-location> #use this command when you need to create a new resource group for your deployment
-New-AzResourceGroupDeployment -ResourceGroupName Demo-AKS-RG -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-azure-bastion/azuredeploy.json
+New-AzResourceGroupDeployment -ResourceGroupName Demo-AKS-RG -TemplateUri https://raw.githubusercontent.com/Joaomanoelc/ARM-Templates/main/Quick-azure-bastion-as-a-service/azuredeploy.json
