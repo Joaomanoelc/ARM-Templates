265de742f6b6d796cee41ee6071068af1f3e2a60
joaomanoel <joaomanoelc@outlook.com>
2020-12-16 06:21:46 -0300

    update

--- recovery-services-vault-create/azuredeploy.ps1
+++ recovery-services-vault-create/azuredeploy.ps1
@@ -2,4 +2,4 @@ New-AzResourceGroup -Name resource-group-name -Location resource-group-location 
 New-AzResourceGroupDeployment -ResourceGroupName resource-group-name -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-recovery-services-vault-create/azuredeploy.json
 
 New-AzResourceGroup -Name resource-group-name -Location resource-group-location #use this command when you need to create a new resource group for your deployment
-New-AzResourceGroupDeployment -ResourceGroupName Demo-AKS-RG -TemplateUri https://raw.githubusercontent.com/Joaomanoelc/ARM-Templates/main/recovery-services-vault-create/azuredeploy.json
+New-AzResourceGroupDeployment -ResourceGroupName resource-group-name -TemplateUri https://raw.githubusercontent.com/Joaomanoelc/ARM-Templates/main/recovery-services-vault-create/azuredeploy.json
